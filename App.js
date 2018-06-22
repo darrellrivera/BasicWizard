571c8061bfffbc88d8086950fa456a94addb4f8b
Darrell Rivera <darrell.rivera@decisiv.com>
2018-06-22 13:52:25 -0400

    Use Wizard component in App.js

--- App.js
+++ App.js
@@ -6,53 +6,29 @@
 
 import React, { Component } from 'react';
 import {
-  Platform,
-  StyleSheet,
-  Text,
+  Alert,
   View
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import Wizard from './src/Wizard';
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
+  handleSubmitWizard = () => {
+    Alert.alert('Wizard has been submitted');
+  }
+
   render() {
+    const steps = [
+      { component: View, routeName: 'Step1' },
+      { component: View, routeName: 'Step2' },
+      { component: View, routeName: 'Step3' },
+    ];
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
+      <Wizard
+        handleSubmitWizard={this.handleSubmitWizard}
+        steps={steps}
+        title='Basic Wizard Example'
+      />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
